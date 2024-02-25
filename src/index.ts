d083d635154d51bd3b096196c18502931dc72dcc
Andres Pradilla <ae.pradillagomez@gmail.com>
2024-02-25 12:49:32 -0800

    Edit package/config/env-variable configurations for production

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,17 @@ import addRoutes from './startup/addRoutes';
 import config from './startup/config'
 import addDocumentation from './startup/addDocumentation';
 import addRequestHandler from './startup/addRequestHandler';
+import addErrorHandler from './startup/addErrorHandler';
 
 const app = express();
 
-const port = parseInt(process.env.PORT as string || "3000", 10);
+const port = parseInt(process.env.NAVBRIX_API_PORT as string || "3000", 10);
 
 addRequestHandler(app)
 config()
 addDocumentation(app, port)
 addMiddleware(app)
 addRoutes(app)
+addErrorHandler(app)
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
